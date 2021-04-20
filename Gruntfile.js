4aa0af24b897165c143f0b64b8f187b5b12b9dd3
Rodrigo Jacques <eng.jacques.yuri@gmail.com>
2021-04-20 19:40:51 -0300

    Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,22 @@ module.exports = function(grunt) {
                }
             }
         },
+
+        sass: {
+          dist: {
+              options: {
+                  //opções de configuração
+              },
+              files: [{
+                          expand: true, //você já sabe o que isso faz
+                          cwd: 'source/sass', // Você também já sabe o que isso faz.
+                          src: ['**/*.scss'], // onde estão os arquivos fontes
+                          dest: 'deploy/css', // pasta de destino.
+                          ext: '.css' // extensão do arquivo fina.
+                      }]
+      
+          }
+      },
     
         uglify: {
             options: {
@@ -65,6 +81,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean'); 
+    grunt.loadNpmTasks('grunt-contrib-sass')
 
     grunt.registerTask('deploy', ['clean', 'autoprefixer', 'cssmin', 'uglify', 'copy'])
 
